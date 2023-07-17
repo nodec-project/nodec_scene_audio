665aeaf5c2636d50f8a799d9cccd4496a415173e
IOE <fivetwothreesix@gmail.com>
2023-07-17 22:10:59 +0900

    import from nodec

---
+++ include/nodec_scene_audio/components/audio_listener.hpp
@@ -0,0 +1,14 @@
+#ifndef NODEC_SCENE_AUDIO__COMPONENTS__AUDIO_LISTENER_HPP_
+#define NODEC_SCENE_AUDIO__COMPONENTS__AUDIO_LISTENER_HPP_
+
+namespace nodec_scene_audio {
+namespace components {
+
+struct AudioListener {
+
+};
+
+} // namespace components
+} // namespace scene_audio
+
+#endif
