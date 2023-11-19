e4815eeb99bca4d3b05718916967ff85a6bebece
IOE <fivetwothreesix@gmail.com>
2023-11-19 18:38:00 +0900

    chore: Transfer codes from nodec

--- include/nodec_scene_audio/components/audio_listener.hpp
+++ include/nodec_scene_audio/components/audio_listener.hpp
@@ -5,10 +5,20 @@ namespace nodec_scene_audio {
 namespace components {
 
 struct AudioListener {
-
+    // TODO: Emitter構造体をprivateなコンポーネントとして作って持たせる
+    // AudioSourceに対するAudioSourceActivity的な奴をつくる
+    // NOTE: Cone用パラメータ,このパラメータ群自体をprivateコンポーネント化した方が良さそう
+    float InnerAngle;
+    float OuterAngle;
+    float InnerVolume;
+    float OuterVolume;
+    float InnerLPF;
+    float OuterLPF;
+    float InnerReverb;
+    float OuterReverb;
 };
 
 } // namespace components
-} // namespace scene_audio
+} // namespace nodec_scene_audio
 
 #endif
